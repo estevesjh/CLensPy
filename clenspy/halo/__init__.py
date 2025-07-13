ad1e45f819e0f6d960a66fec1328956b0f88acec
estevesjh <johnny.udi@gmail.com>
2025-07-13 13:34:16 -0400

    ruff

--- clenspy/halo/__init__.py
+++ clenspy/halo/__init__.py
@@ -2,8 +2,7 @@
 Dark matter halo profiles and related functions.
 """
 
+from . import bias, concentration
 from .nfw import NFWProfile
-from . import bias
-from . import concentration
 
 __all__ = ["NFWProfile", "bias", "concentration"]
